2085c788ee2644c15b3ae106ea5d788d25df0116
KafetzisThomas <tomkafetzis06@gmail.com>
2023-06-24 10:58:08 +0300

    Make syntax changes

--- website_status_checker.py
+++ website_status_checker.py
@@ -6,28 +6,24 @@
 
 import requests
 
-website_url = [
-    "https://www.google.com",
-    "https://www.github.com",
-    "https://www.yahoo.com", 
-    "https://www.amazon.com"
+urls = [
+  "https://www.google.com",
+  "https://www.github.com",
+  "https://www.yahoo.com", 
+  "https://www.amazon.com"
 ]
 
 statuses = {
-    200: "Website Available",
-    301: "Permanent Redirect",
-    302: "Temporary Redirect",
-    404: "Not Found",
-    500: "Internal Server Error",
-    503: "Service Unavailable"
+  200: "Website Available",
+  301: "Permanent Redirect",
+  302: "Temporary Redirect",
+  404: "Not Found",
+  500: "Internal Server Error",
+  503: "Service Unavailable"
 }
 
 print("\nURLs\t\t\t\tMessage\t\t\t\tStatus Code")
 
-for url in website_url:
-    try:
-        web_response = requests.get(url)
-        print(f"{url}\t\t{statuses[web_response.status_code]}\t\t{web_response.status_code}")
-    
-    except:
-        print(f"{url}\t\t{statuses[web_response.status_code]}\t\t{web_response.status_code}")
+for url in urls:
+  web_response = requests.get(url)
+  print(f"{url}\t\t{statuses[web_response.status_code]}\t\t{web_response.status_code}")
