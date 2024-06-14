dc1b506914360e686386ba6721b420b5725f4b28
KafetzisThomas <tomkafetzis06@gmail.com>
2024-06-14 13:59:38 +0300

    Add command line argument

--- main.py
+++ main.py
@@ -4,26 +4,24 @@
 # Project Title: Website-Status-Checker (https://github.com/KafetzisThomas/Website-Status-Checker)
 # Author / Project Owner: KafetzisThomas (https://github.com/KafetzisThomas)
 
+import sys
 import requests
 
-urls = [
-  "https://www.google.com",
-  "https://www.github.com",
-  "https://www.yahoo.com", 
-  "https://www.amazon.com"
-]
-
+# Map HTTP status codes to their corresponding descriptions
 statuses = {
-  200: "Website Available",
-  301: "Permanent Redirect",
-  302: "Temporary Redirect",
-  404: "Not Found",
-  500: "Internal Server Error",
-  503: "Service Unavailable"
+    200: "Website Available",
+    301: "Permanent Redirect",
+    302: "Temporary Redirect",
+    404: "Not Found",
+    500: "Internal Server Error",
+    503: "Service Unavailable",
 }
 
-print("\nURLs\t\t\t\tMessage\t\t\t\tStatus Code")
 
-for url in urls:
-  web_response = requests.get(url)
-  print(f"{url}\t\t{statuses[web_response.status_code]}\t\t{web_response.status_code}")
+url = sys.argv[1]
+
+web_response = requests.get(f"http://www.{url}")
+
+print(
+    f"\nUrl: {url}\nMessage: {statuses[web_response.status_code]}\nStatus code: {web_response.status_code}"
+)
